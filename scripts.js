bc656b5f463b2382ef0bb940d0dd2dd17ae0b365
zektor <dankomajchrak423@gmail.com>
2022-02-11 17:21:56 +0100

    added functionalities, working on the math algorithm now

--- scripts.js
+++ scripts.js
@@ -1,12 +1,27 @@
-function addCharacterToDisplay(){
-    let display_el = document.getElementById("calc-display")
+function addCharacterToDisplay(content){
+    let display_el = document.getElementById("calc-display-numeros");
+    display_el.innerHTML += content;
 }
 
 function addResultToDisplay(){
-
+    let display_el = document.getElementById("calc-display-result");
+    let problem = document.getElementById("calc-display-numeros");
+    display_el.innerHTML += '=';
+    display_el.innerHTML += calculate(problem);
 }
 
-function calculate(){
-
+function calculate(content){
+    
 }
 
+function removeCharacterFromDisplay(){
+    let display_el = document.getElementById("calc-display-numeros");
+    display_el.innerHTML = display_el.innerHTML.slice(0, -1);
+}
+
+function removeAll(){
+    let display_el = document.getElementById("calc-display-numeros");
+    let display_el_result = document.getElementById("calc-display-result");
+    display_el.innerHTML = "";
+    display_el_result.innerHTML = "";
+}
